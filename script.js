f660581365b45822912ce019e47c693330ead908
Rishi Mehta <mehtarishie1@gmail.com>
2017-06-26 15:56:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ module.exports = function(res, query, numb){
 var T = new Twit({
   consumer_key:         '2xZvRzy6ynNoIlLWufvT3Wl5u',
   consumer_secret:      'AlY6yIOsWYQP0EUYuKe6HAblvPWK8IAqhGjLyEfsbcq8pM9NuW',
-  access_token:         '88980301-ZxUJ5OpxuIgnhnKSYOuFnCW5l5t27FL7eooyolYjf',
-  access_token_secret:  'GfwNlkcHPBr2xEThi9lEIVSl2X6e85anrKUxWcK4iINBu',
+  access_token:         '88980301-ZxUJ5OpxuIgnhnKSYOuFnCW5l5t27FL7***yolYjf',
+  access_token_secret:  'GfwNlkcHPBr2xEThi9lEIVSl2X6e85***KUxWcK4iINBu',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
